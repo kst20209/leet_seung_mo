3de07c18c38b690580b484be65576b7b813665c1
Zeropepsi123 <kst20209@gmail.com>
2024-10-02 03:04:41 +0900

    UI바 drop down 형태로 수정

--- lib/screens/problem_solving_page.dart
+++ lib/screens/problem_solving_page.dart
@@ -1,6 +1,7 @@
 export 'problem_solving_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
+import 'dart:ui' as ui;
 import '../models/models.dart';
 
 class ProblemSolvingPage extends StatefulWidget {
@@ -21,17 +22,26 @@ class _ProblemSolvingPageState extends State<ProblemSolvingPage> {
   List<DrawingPoint> currentStroke = [];
   Offset? currentPosition;
   final double eraserWidth = 10.0;
-  bool isColorMenuOpen = false;
-  bool isStrokeWidthMenuOpen = false;
+  OverlayEntry? _colorOverlay;
+  OverlayEntry? _strokeWidthOverlay;
 
-  final List<Color> colors = [
-    Colors.black,
-    Colors.red,
-    Colors.blue,
-    Colors.green,
-    Colors.yellow
-  ];
+  final List<Color> colors = [Colors.black, Colors.red, Colors.blue];
   final List<double> strokeWidths = [1.0, 2.0, 3.0];
+
+  final GlobalKey _toolbarKey = GlobalKey();
+
+  @override
+  void dispose() {
+    _removeOverlays();
+    super.dispose();
+  }
+
+  void _removeOverlays() {
+    _colorOverlay?.remove();
+    _strokeWidthOverlay?.remove();
+    _colorOverlay = null;
+    _strokeWidthOverlay = null;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -62,85 +72,324 @@ class _ProblemSolvingPageState extends State<ProblemSolvingPage> {
           ),
         ],
       ),
-      body: Column(
+      body: GestureDetector(
+        onTap: _removeOverlays,
+        child: Column(
+          children: [
+            _buildToolbar(),
+            Expanded(
+              child: Stack(
+                children: [
+                  Container(color: Colors.white),
+                  Positioned(
+                    left: 20,
+                    top: 20,
+                    child: ConstrainedBox(
+                      constraints: BoxConstraints(
+                        maxWidth: MediaQuery.of(context).size.width - 40,
+                      ),
+                      child: Image.network(
+                        widget.problem.problemImage,
+                        fit: BoxFit.contain,
+                      ),
+                    ),
+                  ),
+                  Listener(
+                    behavior: HitTestBehavior.opaque,
+                    onPointerDown: (PointerDownEvent event) {
+                      if (event.kind == PointerDeviceKind.stylus) {
+                        setState(() {
+                          currentPosition = event.localPosition;
+                          if (isEraserMode) {
+                            _erase(event.localPosition);
+                          } else {
+                            currentStroke = [
+                              DrawingPoint(event.localPosition, selectedColor,
+                                  strokeWidth)
+                            ];
+                          }
+                        });
+                      }
+                    },
+                    onPointerMove: (PointerMoveEvent event) {
+                      if (event.kind == PointerDeviceKind.stylus) {
+                        setState(() {
+                          currentPosition = event.localPosition;
+                          if (isEraserMode) {
+                            _erase(event.localPosition);
+                          } else {
+                            currentStroke.add(DrawingPoint(event.localPosition,
+                                selectedColor, strokeWidth));
+                          }
+                        });
+                      }
+                    },
+                    onPointerUp: (PointerUpEvent event) {
+                      if (event.kind == PointerDeviceKind.stylus) {
+                        setState(() {
+                          currentPosition = null;
+                          if (!isEraserMode) {
+                            strokes.add(List.from(currentStroke));
+                            currentStroke.clear();
+                          }
+                        });
+                      }
+                    },
+                    child: CustomPaint(
+                      painter: DrawingPainter(
+                          strokes,
+                          currentStroke,
+                          isEraserMode,
+                          currentPosition,
+                          strokeWidth,
+                          eraserWidth),
+                      child: Container(
+                        height: double.infinity,
+                        width: double.infinity,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildToolbar() {
+    return Container(
+      key: _toolbarKey,
+      padding: EdgeInsets.all(8.0),
+      color: Colors.grey[200],
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          _buildToolbar(),
-          if (isColorMenuOpen) _buildColorMenu(),
-          if (isStrokeWidthMenuOpen) _buildStrokeWidthMenu(),
-          Expanded(
-            child: Stack(
-              children: [
-                Container(color: Colors.white),
-                Positioned(
-                  left: 20,
-                  top: 20,
-                  child: ConstrainedBox(
-                    constraints: BoxConstraints(
-                      maxWidth: MediaQuery.of(context).size.width - 40,
-                    ),
-                    child: Image.network(
-                      widget.problem.problemImage,
-                      fit: BoxFit.contain,
-                    ),
-                  ),
-                ),
-                Listener(
-                  behavior: HitTestBehavior.opaque,
-                  onPointerDown: (PointerDownEvent event) {
-                    if (event.kind == PointerDeviceKind.stylus) {
-                      setState(() {
-                        currentPosition = event.localPosition;
-                        if (isEraserMode) {
-                          _erase(event.localPosition);
-                        } else {
-                          currentStroke = [
-                            DrawingPoint(
-                                event.localPosition, selectedColor, strokeWidth)
-                          ];
-                        }
-                      });
-                    }
-                  },
-                  onPointerMove: (PointerMoveEvent event) {
-                    if (event.kind == PointerDeviceKind.stylus) {
-                      setState(() {
-                        currentPosition = event.localPosition;
-                        if (isEraserMode) {
-                          _erase(event.localPosition);
-                        } else {
-                          currentStroke.add(DrawingPoint(
-                              event.localPosition, selectedColor, strokeWidth));
-                        }
-                      });
-                    }
-                  },
-                  onPointerUp: (PointerUpEvent event) {
-                    if (event.kind == PointerDeviceKind.stylus) {
-                      currentPosition = null;
-                      setState(() {
-                        strokes.add(List.from(currentStroke));
-                        currentStroke.clear();
-                      });
-                    }
-                  },
-                  child: CustomPaint(
-                    painter: DrawingPainter(
-                        strokes,
-                        currentStroke,
-                        isEraserMode,
-                        currentPosition,
-                        strokeWidth,
-                        eraserWidth),
-                    child: Container(
-                      height: double.infinity,
-                      width: double.infinity,
-                    ),
-                  ),
+          _buildMainColorButton(),
+          SizedBox(width: 20),
+          _buildMainStrokeWidthButton(),
+          SizedBox(width: 20),
+          IconButton(
+            icon: Icon(Icons.edit),
+            color: isEraserMode ? Colors.grey : Colors.black,
+            onPressed: () {
+              setState(() {
+                isEraserMode = false;
+              });
+            },
+          ),
+          SizedBox(width: 20),
+          IconButton(
+            icon: Icon(Icons.cleaning_services),
+            color: isEraserMode ? Colors.red : Colors.black,
+            onPressed: () {
+              setState(() {
+                isEraserMode = true;
+              });
+            },
+          ),
+          SizedBox(width: 20),
+          IconButton(
+            icon: Icon(Icons.settings),
+            onPressed: () {
+              // 설정 기능 구현
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildMainColorButton() {
+    return GestureDetector(
+      onTap: () {
+        _toggleColorMenu();
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: selectedColor,
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey, width: 2),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainStrokeWidthButton() {
+    return GestureDetector(
+      onTap: () {
+        _toggleStrokeWidthMenu();
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(color: Colors.grey, width: 2),
+        ),
+        child: Icon(Icons.circle, size: strokeWidth * 6, color: Colors.black),
+      ),
+    );
+  }
+
+  void _toggleColorMenu() {
+    _removeOverlays();
+    _colorOverlay = _createColorOverlay();
+    Overlay.of(context).insert(_colorOverlay!);
+  }
+
+  void _toggleStrokeWidthMenu() {
+    _removeOverlays();
+    _strokeWidthOverlay = _createStrokeWidthOverlay();
+    Overlay.of(context).insert(_strokeWidthOverlay!);
+  }
+
+  OverlayEntry _createColorOverlay() {
+    RenderBox renderBox =
+        _toolbarKey.currentContext!.findRenderObject() as RenderBox;
+    var offset = renderBox.localToGlobal(Offset.zero);
+    var size = renderBox.size;
+
+    return OverlayEntry(
+      builder: (context) => Positioned(
+        top: offset.dy + size.height,
+        left: 0,
+        right: 0,
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 10,
+                  offset: Offset(0, 3),
                 ),
               ],
             ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: BackdropFilter(
+                filter: ui.ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: colors.map((color) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: _buildColorButton(color),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
           ),
-        ],
+        ),
+      ),
+    );
+  }
+
+  OverlayEntry _createStrokeWidthOverlay() {
+    RenderBox renderBox =
+        _toolbarKey.currentContext!.findRenderObject() as RenderBox;
+    var offset = renderBox.localToGlobal(Offset.zero);
+    var size = renderBox.size;
+
+    return OverlayEntry(
+      builder: (context) => Positioned(
+        top: offset.dy + size.height,
+        left: 0,
+        right: 0,
+        child: Center(
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.2),
+                  spreadRadius: 1,
+                  blurRadius: 10,
+                  offset: Offset(0, 3),
+                ),
+              ],
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(8),
+              child: BackdropFilter(
+                filter: ui.ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                  padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.8),
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: strokeWidths.map((width) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8),
+                        child: _buildStrokeWidthButton(width),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildColorButton(Color color) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedColor = color;
+          _removeOverlays();
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          color: color,
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: selectedColor == color ? Colors.white : Colors.grey,
+            width: 2,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildStrokeWidthButton(double width) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          strokeWidth = width;
+          _removeOverlays();
+        });
+      },
+      child: Container(
+        width: 30,
+        height: 30,
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          color: strokeWidth == width ? Colors.grey[400] : Colors.transparent,
+        ),
+        child: Icon(Icons.circle, size: width * 6, color: Colors.black),
       ),
     );
   }
@@ -174,184 +423,6 @@ class _ProblemSolvingPageState extends State<ProblemSolvingPage> {
           ],
         );
       },
-    );
-  }
-
-  Widget _buildToolbar() {
-    return Container(
-      padding: EdgeInsets.all(8.0),
-      color: Colors.grey[200],
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          _buildMainColorButton(),
-          SizedBox(width: 16),
-          _buildMainStrokeWidthButton(),
-          SizedBox(width: 16),
-          IconButton(
-            icon: Icon(Icons.edit),
-            color: isEraserMode ? Colors.grey : Colors.black,
-            onPressed: () {
-              setState(() {
-                isEraserMode = false;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.cleaning_services),
-            color: isEraserMode ? Colors.red : Colors.black,
-            onPressed: () {
-              setState(() {
-                isEraserMode = true;
-              });
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.settings),
-            onPressed: () {
-              // 설정 기능 구현
-            },
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget _buildMainColorButton() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isColorMenuOpen = !isColorMenuOpen;
-          isStrokeWidthMenuOpen = false;
-        });
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          color: selectedColor,
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.grey, width: 2),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildMainStrokeWidthButton() {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          isStrokeWidthMenuOpen = !isStrokeWidthMenuOpen;
-          isColorMenuOpen = false;
-        });
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          border: Border.all(color: Colors.grey, width: 2),
-        ),
-        child: Icon(Icons.circle, size: strokeWidth * 6, color: Colors.black),
-      ),
-    );
-  }
-
-  Widget _buildColorMenu() {
-    return Container(
-      margin: EdgeInsets.only(top: 8),
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 1,
-            blurRadius: 3,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: colors.map((color) => _buildColorButton(color)).toList(),
-      ),
-    );
-  }
-
-  Widget _buildStrokeWidthMenu() {
-    return Container(
-      margin: EdgeInsets.only(top: 8),
-      padding: EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.only(
-          bottomLeft: Radius.circular(10),
-          bottomRight: Radius.circular(10),
-        ),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.black.withOpacity(0.1),
-            spreadRadius: 1,
-            blurRadius: 3,
-            offset: Offset(0, 2),
-          ),
-        ],
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: strokeWidths
-            .map((width) => _buildStrokeWidthButton(width))
-            .toList(),
-      ),
-    );
-  }
-
-  Widget _buildColorButton(Color color) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          selectedColor = color;
-          isEraserMode = false;
-        });
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        decoration: BoxDecoration(
-          color: color,
-          shape: BoxShape.circle,
-          border: Border.all(
-            color: selectedColor == color ? Colors.white : Colors.grey,
-            width: 2,
-          ),
-        ),
-      ),
-    );
-  }
-
-  Widget _buildStrokeWidthButton(double width) {
-    return GestureDetector(
-      onTap: () {
-        setState(() {
-          strokeWidth = width;
-          isStrokeWidthMenuOpen = false;
-        });
-      },
-      child: Container(
-        width: 30,
-        height: 30,
-        margin: EdgeInsets.symmetric(horizontal: 4),
-        decoration: BoxDecoration(
-          shape: BoxShape.circle,
-          color: strokeWidth == width ? Colors.grey[400] : Colors.transparent,
-        ),
-        child: Icon(Icons.circle, size: width * 6, color: Colors.black),
-      ),
     );
   }
 
