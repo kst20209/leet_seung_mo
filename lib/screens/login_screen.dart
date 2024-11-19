7cdbdf364865080d5350cf4786e26a0505d341f9
Zeropepsi123 <kst20209@gmail.com>
2024-11-19 16:06:26 +0900

    미사용 구문 warning 간단하게 수정

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 import '../providers/auth_provider.dart';
-import '../providers/user_data_provider.dart';
 import 'signup/signup_screen.dart';
 
 class LoginScreen extends StatefulWidget {
