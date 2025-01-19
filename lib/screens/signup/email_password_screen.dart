3d31aed30b54785ca9859209f47c1a8f29e56855
kst20209 <kst20209@naver.com>
2025-01-19 22:06:05 +0900

    회원가입 및 로그인 UI 약간 수정함

--- lib/screens/signup/email_password_screen.dart
+++ lib/screens/signup/email_password_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:leet_seung_mo/utils/firebase_service.dart';
+import 'package:leet_seung_mo/utils/responsive_container.dart';
 import 'package:leet_seung_mo/utils/user_repository.dart';
 import '../../widgets/build_text_field.dart';
 
@@ -64,86 +65,88 @@ class _EmailPasswordScreenState extends State<EmailPasswordScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      BuildTextField(
-                        context: context,
-                        controller: _emailController,
-                        keyboardType: TextInputType.emailAddress,
-                        label: '이메일',
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '이메일을 입력해주세요';
-                          }
-                          if (_emailError != null) {
-                            final error = _emailError;
-                            _emailError =
-                                null; // Clear the error after showing it once
-                            return error;
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 16),
-                      BuildTextField(
-                        context: context,
-                        controller: _passwordController,
-                        label: '비밀번호',
-                        obscureText: true,
-                        validator: (value) {
-                          if (value == null || value.isEmpty) {
-                            return '비밀번호를 입력해주세요';
-                          }
-                          if (!isPasswordStrong(value)) {
-                            return '비밀번호는 6-15자이며, 숫자와 특수문자를 포함해야 합니다';
-                          }
-                          return null;
-                        },
-                      ),
-                      SizedBox(height: 16),
-                      BuildTextField(
-                        context: context,
-                        controller: _confirmPasswordController,
-                        label: '비밀번호 확인',
-                        obscureText: true,
-                        validator: (value) {
-                          if (value != _passwordController.text) {
-                            return '비밀번호가 일치하지 않습니다';
-                          }
-                          return null;
-                        },
-                      ),
-                    ],
+      body: ResponsiveContainer(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        BuildTextField(
+                          context: context,
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          label: '이메일',
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '이메일을 입력해주세요';
+                            }
+                            if (_emailError != null) {
+                              final error = _emailError;
+                              _emailError =
+                                  null; // Clear the error after showing it once
+                              return error;
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16),
+                        BuildTextField(
+                          context: context,
+                          controller: _passwordController,
+                          label: '비밀번호',
+                          obscureText: true,
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return '비밀번호를 입력해주세요';
+                            }
+                            if (!isPasswordStrong(value)) {
+                              return '비밀번호는 6-15자이며, 숫자와 특수문자를 포함해야 합니다';
+                            }
+                            return null;
+                          },
+                        ),
+                        SizedBox(height: 16),
+                        BuildTextField(
+                          context: context,
+                          controller: _confirmPasswordController,
+                          label: '비밀번호 확인',
+                          obscureText: true,
+                          validator: (value) {
+                            if (value != _passwordController.text) {
+                              return '비밀번호가 일치하지 않습니다';
+                            }
+                            return null;
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            padding: const EdgeInsets.all(24.0),
-            child: ElevatedButton(
-                child: Text('다음', style: TextStyle(fontSize: 18)),
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Theme.of(context).primaryColor,
-                  padding: EdgeInsets.symmetric(vertical: 15),
-                  minimumSize: Size(double.infinity, 50),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(24.0),
+              child: ElevatedButton(
+                  child: Text('다음', style: TextStyle(fontSize: 18)),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Theme.of(context).primaryColor,
+                    padding: EdgeInsets.symmetric(vertical: 15),
+                    minimumSize: Size(double.infinity, 50),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
                   ),
-                ),
-                onPressed: _createAccount),
-          ),
-        ],
+                  onPressed: _createAccount),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/screens/signup/nickname_screen.dart
+++ lib/screens/signup/nickname_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:leet_seung_mo/utils/responsive_container.dart';
 import '../../widgets/build_text_field.dart';
 import '../../utils/user_repository.dart';
 import '../../utils/firebase_service.dart';
@@ -31,75 +32,77 @@ class _NicknameScreenState extends State<NicknameScreen> {
         backgroundColor: Theme.of(context).scaffoldBackgroundColor,
         elevation: 0,
       ),
-      body: Column(
-        children: [
-          Expanded(
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(24.0),
-                child: Form(
-                  key: _formKey,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      _buildSectionTitle('닉네임'),
-                      NicknameTextFieldWithButton(
-                        controller: _nicknameController,
-                        onPressed: _checkNicknameDuplicate,
-                        validator: _validateNickname,
-                        isNicknameAvailable: _isNicknameAvailable,
-                      ),
-                      SizedBox(height: 32),
-                      _buildSectionTitle('성별'),
-                      _buildSelectionWidget(
-                        options: ['남', '여', '무응답'],
-                        selectedOption: _selectedGender,
-                        onSelect: (value) =>
-                            setState(() => _selectedGender = value),
-                      ),
-                      SizedBox(height: 24),
-                      _buildSectionTitle('직업'),
-                      _buildSelectionWidget(
-                        options: ['학생', '대졸', '직장인', '무응답'],
-                        selectedOption: _selectedOccupation,
-                        onSelect: (value) =>
-                            setState(() => _selectedOccupation = value),
-                      ),
-                      SizedBox(height: 24),
-                      _buildSectionTitle('서비스 인지 경로'),
-                      _buildSelectionWidget(
-                        options: [
-                          'Instagram 광고',
-                          '인터넷 검색',
-                          '지인 추천',
-                          '앱스토어',
-                          '기타',
-                          '무응답'
-                        ],
-                        selectedOption: _selectedDiscoveryMethod,
-                        onSelect: (value) =>
-                            setState(() => _selectedDiscoveryMethod = value),
-                      ),
-                    ],
+      body: ResponsiveContainer(
+        child: Column(
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(24.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        _buildSectionTitle('닉네임'),
+                        NicknameTextFieldWithButton(
+                          controller: _nicknameController,
+                          onPressed: _checkNicknameDuplicate,
+                          validator: _validateNickname,
+                          isNicknameAvailable: _isNicknameAvailable,
+                        ),
+                        SizedBox(height: 32),
+                        _buildSectionTitle('성별'),
+                        _buildSelectionWidget(
+                          options: ['남', '여', '무응답'],
+                          selectedOption: _selectedGender,
+                          onSelect: (value) =>
+                              setState(() => _selectedGender = value),
+                        ),
+                        SizedBox(height: 24),
+                        _buildSectionTitle('직업'),
+                        _buildSelectionWidget(
+                          options: ['학생', '대졸', '직장인', '무응답'],
+                          selectedOption: _selectedOccupation,
+                          onSelect: (value) =>
+                              setState(() => _selectedOccupation = value),
+                        ),
+                        SizedBox(height: 24),
+                        _buildSectionTitle('서비스 인지 경로'),
+                        _buildSelectionWidget(
+                          options: [
+                            'Instagram 광고',
+                            '인터넷 검색',
+                            '지인 추천',
+                            '앱스토어',
+                            '기타',
+                            '무응답'
+                          ],
+                          selectedOption: _selectedDiscoveryMethod,
+                          onSelect: (value) =>
+                              setState(() => _selectedDiscoveryMethod = value),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          Container(
-            width: double.infinity,
-            child: ElevatedButton(
-              child: Text('가입 완료', style: TextStyle(fontSize: 18)),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Theme.of(context).primaryColor,
-                padding: EdgeInsets.symmetric(vertical: 15),
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(10)),
+            Container(
+              width: double.infinity,
+              child: ElevatedButton(
+                child: Text('가입 완료', style: TextStyle(fontSize: 18)),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Theme.of(context).primaryColor,
+                  padding: EdgeInsets.symmetric(vertical: 15),
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                ),
+                onPressed: _submitForm,
               ),
-              onPressed: _submitForm,
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
